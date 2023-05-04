184948b6b072682f5e9826dd49f12d5ff8d72436
Saiyannaing23 <pesilay2372002@gmail.com>
2023-05-05 01:40:09 +0630

    Header and Hero

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// ---------- Typing ------------------
+var typingEffect = new Typed(".multi-text",{
+    strings : [" Coder."," Web Developer."],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 1500
+})
